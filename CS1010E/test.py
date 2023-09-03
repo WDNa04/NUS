0694133985591ee1b1131a1361016f3b512cd93d
WDNa04 <WNa0531@outlook.com>
2023-09-04 01:49:06 +0800

    hi

--- CS1010E/test.py
+++ CS1010E/test.py
@@ -1,9 +1,9 @@
-from turtle import *
-
-fd(100)
-rt(90)
-fd(100)
-rt(90)
-fd(100)
-rt(90)
-fd(100)
+def check_age():
+    i = 0
+    while True: 
+        age = int(input("Enter age: "))
+        i += 1
+        if int(age) >= 1 and int(age) <= 100:
+            print('Your age is ' + str(age))
+            print('Number of attempts = ' +str(i))
+check_age()
